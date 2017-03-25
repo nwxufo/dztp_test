9fc9104e9484ec47761bbd73caad02b17e4f4014
William <printer_bzu@163.com>
2017-03-25 16:14:04 +0800

    finished basic response framework. next depath it to differ file.

--- main.c
+++ main.c
@@ -42,9 +42,7 @@ struct response_struct {
 	struct dzt_protocol dztp;
 };
 
-
-static void dzt_proto_printer( struct dzt_protocol dztp ) ;
-static struct dzt_protocol msg_to_protocol_struct( const char *msg )
+static struct dzt_protocol init_dzt_protocol( const char *msg )
 {
 	struct dzt_protocol dztp;
 	/* encapsulation the msg to protocol struct. */
@@ -59,11 +57,10 @@ static struct dzt_protocol msg_to_protocol_struct( const char *msg )
 		dztp.param.param[j] = msg[4+j];
 		j++;
 	}
-	dztp.checkout = msg[4+i];//TODO: jiaoyan
+	dztp.checkout = msg[4+i];//TODO: CRC
 	dztp.end = msg[4+i+1];
 
 	return dztp;
-	
 }
 
 static void dzt_proto_printer( struct dzt_protocol dztp ) 
@@ -86,7 +83,7 @@ static void dzt_proto_printer( struct dzt_protocol dztp )
 
 }
 
-static void msg_response_recovery(int fd) {
+static void msg_response_recovery(struct response_struct res_obj) {
 	unsigned int isSucceed = 1;
 	unsigned char msg_res[7];
 		msg_res[0] = 0x3c;
@@ -101,32 +98,63 @@ static void msg_response_recovery(int fd) {
 			msg_res[5] = 0xB3;
 		}
 		msg_res[6] = 0x3E;
-	write(fd, msg_res, 7);	
+	write(res_obj.fd, msg_res, 7);	
 }
-static void msg_response_version() 
+static void msg_response_version(struct response_struct res_obj) 
 {
-	printf("reviced checkout version message !\n");
+	printf("reviced version request  message !\n");
 }
+static void msg_response_operation(struct response_struct res_obj)
+{
+	printf("reviced operation message !\n");
+}
+static void msg_response_checkout(struct response_struct res_obj)
+{
+	printf("reviced checkout message !\n");
+}
+
+void (*msg_response_tbl[])(struct response_struct) = {
+	msg_response_recovery,
+	msg_response_version,
+	msg_response_operation,
+	msg_response_checkout
+};
+#define msg_response(z) msg_response_tbl[z.type](z)
 
 static struct response_struct init_response_struct( int fd, const char* msg )
 {
 	struct response_struct res_obj;
 	res_obj.fd = fd;
-	res_obj.dztp = msg_to_protocol_struct(msg);
+	res_obj.dztp = init_dzt_protocol(msg);
 	if ( 0x05 == res_obj.dztp.cmd ) {
 		res_obj.type = RECOVERY;
 	} else 
 	if ( 0x81 == res_obj.dztp.cmd ) {
 		res_obj.type = VERSION_CHECK; 
 	} else
-	if ( 0x50 == res_obj.dztp.cmd & 0x50 ) {
+	if ( 0x50 == (res_obj.dztp.cmd & 0x50) ) {
 		res_obj.type = OPERATION;
 	} else 
-	if ( 0xA0 == res_obj.dztp.cmd & 0xA0 ) {
+	if ( 0xA0 == ( res_obj.dztp.cmd & 0xA0 ) ) {
 		res_obj.type = CHECKOUT;
 	} else {
 		printf( "cmd unrecognised !\n" );
-		//TODO:print it!
+		//protocol_crc(msg);
+#ifdef DEBUG
+		printf("cmd & 0xA0: %.04x\n", res_obj.dztp.cmd & 0xA0 );
+#endif
+		//print it!
+		printf("%c ",msg[0]);
+		printf("%.4x ", msg[1]);
+		int len = (int)msg[1];
+		int i = 0;
+		while( i< len ) {
+			printf("%.4x ",msg[i+2]);
+			i++;
+		}
+		printf("%c\n", msg[len+2]);
+		//dzt protocol printer.
+		dzt_proto_printer(res_obj.dztp);
 		exit(-1);
 	}
 
@@ -140,17 +168,9 @@ static void  msg_processor(int fd, const char* msg)
 	res_obj = init_response_struct( fd, msg);
 	//dztp = msg_translator(res_obj,msg);
 	dzt_proto_printer( res_obj.dztp );
-	if ( 0x05 == res_obj.dztp.cmd & 0x05 ) { // device repositon
-		msg_response_recovery(res_obj.fd);
-	} else if ( 0x81 == res_obj.dztp.cmd ) {
-		msg_response_version();
-	} else if ( 0xA0 == res_obj.dztp.cmd & 0xA0 ) {
-		msg_response_checkout();
-	} else {
-		printf("received cmd unrecognizable !\n");
-	}
-}
 
+	msg_response(res_obj);
+}
 void main(int argc, char *argv[])
 {
 	char message[BUF_LEN];
